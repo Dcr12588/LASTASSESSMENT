8700c5962391dc04d98d5c0ce1767a409ff8af6e
Danny Ramirez <dcr1999jr@gmail.com>
2022-10-01 16:32:53 -0700

    ASSESSMENT

--- runtime.js
+++ runtime.js
@@ -47,16 +47,16 @@ const extraLargeArray = getSizedArray(100000);
 
 // Try it with first function
 perf.start();                     // Starts timer
-doublerAppend(tinyArray);
+doublerAppend(extraLargeArray);
 let resultsAppend = perf.stop();  // Stops timer and save time results
 
 
 // Try it with second function
 perf.start();
-doublerInsert(tinyArray);
+doublerInsert(extraLargeArray);
 let resultsInsert = perf.stop();
 
 
-console.log('Results for the tinyArray');
+console.log('Results for the extraLargeArray');
 console.log("insert", resultsInsert.preciseWords);
 console.log("append", resultsAppend.preciseWords);
